5682342b2e2e7a3923d0110763010fb7632fce4c
Dusica Krstic <dusicamkrstic.com>
2018-01-03 18:44:26 +0100

    Zavrseno iscrtavanje unete niske.

--- src/Main.hs
+++ src/Main.hs
@@ -2,15 +2,15 @@ module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
+import Data.String
 
 width, height, offset :: Int
 width = 600
 height = 600
 offset = 100
-traceLine = []
 
 fps :: Int
-fps = 60
+fps = 1
 
 window :: Display
 window = InWindow "Trace Tracker" (width, height) (offset, offset)
@@ -20,41 +20,45 @@ background = black
 
 data DrawMode = Mode {
     pointA :: (Float, Float),
-    pointB :: (Float, Float)
+    pointB :: (Float, Float),
+    dirLine :: String
 } deriving Show
 
-direction :: String -> Char
-direction [] = 'F'
-direction (x:xs) = x  
+direction :: String -> String
+direction [] = "F"
+direction (x:xs) = [x]
 
-initialState :: DrawMode
-initialState = Mode {
+initialState :: String -> DrawMode
+initialState traceLine = Mode {
     pointA = (0, 0),
-    pointB = (0, 0)
+    pointB = (0, 0),
+    dirLine = traceLine
 }
 
 render :: DrawMode -> Picture
 render mode = pictures [trace]
               where
-                trace = color ballColor $ Line [pointA mode, pointB mode]
-                ballColor = dark red
+                trace = color lineColor $ Line [pointA mode, pointB mode]
+                lineColor = dark red
 
 moveLine :: Float -> DrawMode -> DrawMode
-moveLine sec mode = mode { pointA = pointB mode, pointB = (x, y)} 
-   where
-    dir = direction traceLine
-    (x, y) = if dir == 'U'
-             then (fst $ pointB mode, snd $ pointB mode + 25) 
-             else if dir == 'D'
+moveLine seconds mode = mode { pointA = pointB mode, pointB = (x, y), dirLine = rest (dirLine mode)} 
+   where 
+       dir = direction (dirLine mode)
+       (x, y) = if dir == "U" 
+            then (fst $ pointB mode, snd $ pointB mode + 25) 
+            else if dir == "D"
                   then (fst $ pointB mode, snd $ pointB mode - 25) 
-             else if dir == 'R'
+            else if dir == "R"
                   then (fst $ pointB mode + 25, snd $ pointB mode)
-             else (fst $ pointB mode - 25, snd $ pointB mode)
-
-     
+                  else (fst $ pointB mode - 25, snd $ pointB mode)    
+                  
+rest :: String -> String
+rest  [] = []
+rest (x:xs) = xs
 
 update :: ViewPort -> Float -> DrawMode -> DrawMode
-update _ = moveLine
+update _ seconds = moveLine seconds
 
             
 checkString :: String -> Bool
@@ -62,13 +66,16 @@ checkString [] = True
 checkString (x:xs) = if x == 'U' || x == 'D' || x == 'R' || x == 'L'
                    then checkString xs 
                    else False    
-                
+
+callSim traceLine = simulate window background fps (initialState traceLine) render update                    
+                   
 main :: IO ()
 main = do  
     putStrLn "Unesite nisku za iscrtavanje:"  
-    traceLine <- getLine  
+    traceLine <- getLine 
     if null traceLine
     then putStrLn "Trazi se niska oblika UDLR (U-up, D-down, L-left, R-right)" 
     else if checkString traceLine
-        then simulate window background fps initialState render update
+        then callSim traceLine
         else putStrLn "Neispravna niska. Pokusajte ponovo"
+        
